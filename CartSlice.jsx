6cb357411ca1367ede6132fe5bed4252a150a8b4
PUNEET KUMAR <kumarmahesh3594@gmail.com>
2026-03-10 00:05:10 +0530

    Refactor cartSlice to use object for initial state

--- CartSlice.jsx
+++ CartSlice.jsx
@@ -2,13 +2,34 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
   name: "cart",
-  initialState: [],
+  initialState: {
+    items: []
+  },
+
   reducers: {
+
     addItem: (state, action) => {
-      state.push(action.payload);
+      state.items.push(action.payload);
+    },
+
+    removeItem: (state, action) => {
+      state.items = state.items.filter(
+        item => item.id !== action.payload
+      );
+    },
+
+    updateQuantity: (state, action) => {
+      const item = state.items.find(
+        item => item.id === action.payload.id
+      );
+
+      if(item){
+        item.quantity = action.payload.quantity;
+      }
     }
+
   }
 });
 
-export const { addItem } = cartSlice.actions;
-export default cartSlice.reducer;
+export const { addItem, removeItem, updateQuantity } = cartSlice.actions;
+export default cartSlice.reducer;;

0301e693d895dc7f897011709969ad2628db60e2
PUNEET KUMAR <kumarmahesh3594@gmail.com>
2026-03-09 23:45:08 +0530

    AboutUs component for Paradise Nursery

---
+++ AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery provides a wide variety of indoor plants.
+        Our mission is to bring greenery and freshness into every home.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
